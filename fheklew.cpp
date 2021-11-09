a0daf6d9692fd38d038f744cd42f0b2eb573ce7a
senabilgin <49808970+senabilgin@users.noreply.github.com>
2021-11-09 17:43:34 +0300

    Add files via upload

---
+++ fheklew.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int yil,bugun,yas;
+	bugun=2021;
+	
+	cout<<"dogum yilinizi giriniz"<<endl;
+	cin>>yil;
+	
+	yas=bugun-yil;
+	
+	cout<<"suanki yasiniz "<<yas<<" dir"<<endl;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
